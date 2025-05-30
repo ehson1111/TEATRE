6229253b0344b85755ad5ec59019c9cf52a61673
Rezvon <rezvon0880@gmail.com>
2025-05-30 12:07:26 +0300

    create

--- cinema/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import MovieListView, MovieCreateView, MovieUpdateView, MovieDeleteView
-
-urlpatterns = [
-    path('', MovieListView.as_view(), name='movie_list'),
-    path('create/', MovieCreateView.as_view(), name='movie_create'),
-    path('update/<int:pk>/', MovieUpdateView.as_view(), name='movie_update'),
-    path('delete/<int:pk>/', MovieDeleteView.as_view(), name='movie_delete'),
-]
